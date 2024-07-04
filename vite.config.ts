13f9666c32beb9338171b35dfab1e1a18bc677c6
youxt-njnu <10190208@njnu.edu.cn>
2024-07-04 23:32:57 +0800

    0704 final

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ABOUT_PORTFOLIO/',
+  base: './',
   assetsInclude: ['**/*.glb'],
   build: {
     chunkSizeWarningLimit: 1500
